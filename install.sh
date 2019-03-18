656ac15faaa1b8ca7ed1aafb3e7d8b633bb04d2a
Rodrigo Ceccato de Freitas <rodrigoceccatodefreitas@gmail.com>
2019-03-18 13:33:33 -0300

    added screen info and changed some files name

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 echo 'Adding vim configuration...'
 echo 'The plugins install requires CURL'
-cp generate.vim ~/.vimrc
+cp vimrc.txt ~/.vimrc
+
+echo "Adding screen configuration..."
+echo "WARNING: this only copies the files, you must have sreen previosly installed"
+cp screenrc.txt ~/.screenrc
 
